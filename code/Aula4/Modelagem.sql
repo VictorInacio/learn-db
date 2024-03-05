95bdb6cd8d67fd87447ad1b2cdda58ad5bb5a139
Victor Inacio <victorinacio@gmail.com>
2024-03-05 09:53:27 -0300

    Aula4 - final

--- code/Aula4/Modelagem.sql
+++ code/Aula4/Modelagem.sql
@@ -6,15 +6,53 @@ CREATE TABLE Cliente (
     telefone VARCHAR(50)
 );
 
+INSERT INTO Cliente (nome, endereco, telefone) 
+VALUES ('Cliente 2', 'Rua B', '11 123131212');
+
+
+INSERT INTO Cliente (nome, endereco, telefone) 
+VALUES ('Cliente 3', 'Rua C', '11 123131212');
+
+
+INSERT INTO Cliente (cliente_id, nome, endereco, telefone) 
+VALUES (4, 'Cliente 4', 'Rua D', '11 123131212');
+
+
+INSERT INTO Cliente (nome, endereco, telefone) 
+VALUES ('Cliente 5', 'Rua E', '11 123131212');
+
+SELECT setval('cliente_cliente_id_seq', 78, true);  
+
+
+INSERT INTO Cliente (nome, endereco, telefone) 
+VALUES ('Cliente 6', 'Rua 6', '11 123131212');
+
+SELECT setval('cliente_cliente_id_seq', 6, true); 
+
+
+INSERT INTO Cliente (nome, endereco, telefone) 
+VALUES ('Cliente 7', 'Rua 7', '11 123131212');
+
+
 -- Criação da tabela Contrato
 CREATE TABLE Contrato (
     contrato_id SERIAL PRIMARY KEY,
-    cliente_id INT NOT NULL,
-    data_inicio DATE NOT NULL,
-    data_fim DATE,
+    cliente_id INT NOT NULL, -- FOREIGN KEY
+    data_inicio VARCHAR(255) NOT NULL,
+    data_fim VARCHAR(255),
     valor DECIMAL(10, 2) NOT NULL,
     FOREIGN KEY (cliente_id) REFERENCES Cliente(cliente_id)
 );
+
+INSERT INTO Contrato (--contrato_id, 
+cliente_id, data_inicio, data_fim, valor ) 
+VALUES (1, '01/01/2024', '31/12/2024', 1000);
+
+INSERT INTO Contrato (--contrato_id, 
+cliente_id, data_inicio, data_fim, valor ) 
+VALUES (79, '01/01/2024', '31/12/2024', 500);
+
+-- INTEGRIDADE REFERENCIAL
 
 -- Criação da tabela Equipamento
 CREATE TABLE Equipamento (
